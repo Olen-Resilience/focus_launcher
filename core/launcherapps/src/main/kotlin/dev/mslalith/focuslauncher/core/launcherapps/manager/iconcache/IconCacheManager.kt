853e54bfa81d0eb5ebc703f6c20a08d628f95232
Olen <olen.and.resilience@gmail.com>
2026-02-28 12:48:35 +0500

    Update IconCacheManager.kt

--- core/launcherapps/src/main/kotlin/dev/mslalith/focuslauncher/core/launcherapps/manager/iconcache/IconCacheManager.kt
+++ core/launcherapps/src/main/kotlin/dev/mslalith/focuslauncher/core/launcherapps/manager/iconcache/IconCacheManager.kt
@@ -5,7 +5,7 @@ import dev.mslalith.focuslauncher.core.launcherapps.parser.IconPackXmlParser
 import dev.mslalith.focuslauncher.core.model.IconPackType
 import dev.mslalith.focuslauncher.core.model.app.AppWithComponent
 
-internal interface IconCacheManager {
+interface IconCacheManager {
     fun clearCache()
     fun iconPackFor(packageName: String): IconPackXmlParser
     fun iconFor(appWithComponent: AppWithComponent, iconPackType: IconPackType): Drawable
